44595eb8a49751406eeb74a35e79d6013f7a2c17
Kakujo <kakujomics.media@gmail.com>
2023-11-06 13:20:33 -0800

    solve lesson 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(phrase) {
+    let reversePhrase = "";
+    for (let i = phrase.length; i >= 0; i--) {
+        reversePhrase += phrase.charAt(i);
+    }
 
+    return reversePhrase;
 };
 
 // Do not edit below this line

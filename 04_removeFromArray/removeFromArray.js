f9ab3f6fae4dee4b7ce980ad64cfda0b36bb5d3a
Kakujo <kakujomics.media@gmail.com>
2023-11-06 14:15:25 -0800

    solve lesson 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(a,...toRemove) {
+    for (let i = 0; i < a.length; i++) {
+        for (let j = 0; j < toRemove.length; j++) {
+            if (a[i] === toRemove[j]) {
+                a.splice(i,1);
+                i--;
+            }
+            
+        }
+            
+    }
+    return a;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;

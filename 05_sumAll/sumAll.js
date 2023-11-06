331d5bcf45cb6eac3a51d7863c73e92703f4f6b6
Kakujo <kakujomics.media@gmail.com>
2023-11-06 14:28:32 -0800

    solve lesson 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || typeof a !== "number" || typeof b !== "number") {
+        return "ERROR";
+    }
+    let sortedNumbers = [a,b].sort();
+    let sum = 0;
+    for (let i = sortedNumbers[0]; i <= sortedNumbers[1]; i++ ) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
